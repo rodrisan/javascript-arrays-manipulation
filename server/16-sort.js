9271a76e78ccb5df3a9f98b7d45b765fd6c52d32
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2023-06-10 11:31:30 -0600

    feat(): working with sort function ordering dates

--- server/16-sort.js
+++ server/16-sort.js
@@ -52,3 +52,34 @@ orders.sort();
 console.log("orders", orders);
 orders.sort((a, b) => b.total - a.total);
 console.log("orders sorted by total desc", orders);
+
+// Sort by Date
+
+const ordersWithDate = [
+  {
+    customerName: "Nicolas",
+    total: 600,
+    delivered: true,
+    date: new Date (2021, 3, 8, 4),
+  },
+  {
+    customerName: "Zulema",
+    total: 120,
+    delivered: false,
+    date: new Date (2021, 9, 12, 3),
+  },
+  {
+    customerName: "Santiago",
+    total: 1840,
+    delivered: true,
+    date: new Date (2021, 8, 2, 2),
+  },
+  {
+    customerName: "Valentina",
+    total: 240,
+    delivered: true,
+    date: new Date(2021, 1, 1, 9, 30),
+  },
+];
+ordersWithDate.sort((a,b) => b.date - a.date);
+console.log(ordersWithDate);
