3f66312aaa22d1f8d304d81553502cc90f3a4c08
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2023-06-10 10:07:24 -0600

    feat(): creating flat logic using recursion

--- server/13-flat.js
+++ server/13-flat.js
@@ -20,3 +20,44 @@ console.log("flat", flat);
 
 const flatWithDepth = arrayOfArrays.flat(2);
 console.log("flat with depth 2", flatWithDepth);
+
+// Recursive solution
+
+const arrayOfArrays2 = [
+  [1, 2, 3, [11, 12, 13]],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+const arrayFlatter = (array, returnArray = []) => {
+  array.forEach((element) => {
+    Array.isArray(element)
+      ? arrayFlatter(element, returnArray)
+      : returnArray.push(element);
+  });
+  return returnArray;
+
+  // This works too.
+
+  // for (let i = 0; i < array.length; i++) {
+  //   const element = array[i];
+  //   if (Array.isArray(element)) {
+  //     arrayFlatter(element, returnArray);
+  //   } else {
+  //     returnArray.push(element)
+  //   }
+  // }
+  // return returnArray;
+
+  // This works too.
+
+  // array.forEach((element) => {
+  //   if (Array.isArray(element)) {
+  //     arrayFlatter(element, returnArray);
+  //   } else {
+  //     returnArray.push(element);
+  //   }
+  // });
+  // return returnArray;
+};
+console.log("recursive array flatter", arrayFlatter(arrayOfArrays2));
